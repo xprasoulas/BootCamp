3f9f07bc34afcef1d16abff0512887780511a8ad
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 21:42:23 +0200

    Create/Fill Course/Student Class

---
+++ BootCampApp/BootCampApp/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BootCampApp.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Hours { get; set; }
+
+        //TODO List Enrollments
+    }
+}
---
+++ BootCampApp/BootCampApp/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BootCampApp.Models
+{   /// <summary>
+    ///  A student can enroll in Many Classes
+    ///  A class has many students
+    /// </summary>
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        
+        //todo list Enrollments
+    }
+}
