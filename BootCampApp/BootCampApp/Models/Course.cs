cf9ee9fe655687c6cb3cc9c5145ae11f5b22ae18
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 21:52:34 +0200

    non auto increment property

--- BootCampApp/BootCampApp/Models/Course.cs
+++ BootCampApp/BootCampApp/Models/Course.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;//
 
 namespace BootCampApp.Models
 {
     public class Course
     {
+        [DatabaseGenerated[DatabaseGeneratedOption.None]]//non auto increment property
         public int CourseID { get; set; }
         public string Title { get; set; }
         public int Hours { get; set; }
