dd2e829c3606473d00434e38aae377390b66046b
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 22:36:35 +0200

    Create relation between Enrollment- Student(1-1) and Enrollments -Class (1-1)

--- BootCampApp/BootCampApp/Models/Enrollment.cs
+++ BootCampApp/BootCampApp/Models/Enrollment.cs
@@ -8,8 +8,10 @@
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
-        public Grade? Grade { get; set; } //nullable property
+        public Grade? Grade { get; set; } //nullable property- attribute for class
 
-        //TODO Student, Course
+        //
+        public virtual Student Student { get; set; } //1-1 Relation = 1 enrollment have 1 student
+        public virtual Course Course { get; set; } // for One Class 1-1 Relation = 1 enrollment have 1 class
     }
 }
