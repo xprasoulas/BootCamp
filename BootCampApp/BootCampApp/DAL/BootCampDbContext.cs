f7061014d8157e131a56591513f931acdd0c4d0f
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 23:13:30 +0200

    Fill DbContext with the tables

--- BootCampApp/BootCampApp/DAL/BootCampDbContext.cs
+++ BootCampApp/BootCampApp/DAL/BootCampDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BootCampApp.Models;//
+using System;
 using System.Collections.Generic;
 using System.Data.Entity; //
 using System.Linq;
@@ -8,5 +9,13 @@ namespace BootCampApp.DAL
 {
     public class BootCampDbContext : DbContext
     {
+        public BootCampDbContext() : base("BootCampContext")
+        { }
+
+        // Fill it with the table that we want to create
+        
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
     }
 }
