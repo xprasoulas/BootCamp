734705fc28a0d9f9cff9d88855f82eb8069d738a
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 23:29:46 +0200

    Add an "optional"convention to remove from tables the plural name

--- BootCampApp/BootCampApp/DAL/BootCampDbContext.cs
+++ BootCampApp/BootCampApp/DAL/BootCampDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity; //
 using System.Linq;
 using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;//
 
 namespace BootCampApp.DAL
 {
@@ -17,5 +18,10 @@ namespace BootCampApp.DAL
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) //override On.. press "Enter"
+        {
+            modelBuilder.Conventions.Remove<PluralizingEntitySetNameConvention>(); //convention to remove from tables the plural name
+        }
     }
 }
