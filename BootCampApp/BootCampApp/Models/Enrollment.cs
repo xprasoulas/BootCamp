bffad7b7cd22ff82ced8d89b592900c0a532daa3
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-24 22:04:13 +0200

    Create/Fill Class Enrollment

---
+++ BootCampApp/BootCampApp/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+﻿namespace BootCampApp.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, E, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public Grade? Grade { get; set; } //nullable property
+
+        //TODO Student, Course
+    }
+}
