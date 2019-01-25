e97df906e0cc77b4cda26baaf95640d368ef906c
Charis Prasoulas <xprasoulas@gmail.com>
2019-01-25 22:22:01 +0200

    Initial Data of Db for Coures & Enrollment using seed method and save changes to db

--- BootCampApp/BootCampApp/DAL/BootCampInitializer.cs
+++ BootCampApp/BootCampApp/DAL/BootCampInitializer.cs
@@ -57,6 +57,125 @@ namespace BootCampApp.DAL
 
             students.ForEach(s => context.Students.Add(s)); //Convert each data to records 
             context.SaveChanges();
+            var courses = new List<Course>
+            {
+                new Course
+                {
+                    CourseID = 1050,
+                    Title = "OOP",
+                    Hours = 851
+                },
+                new Course
+                {
+                    CourseID = 1060,
+                    Title = "C#",
+                    Hours = 905
+                },
+                new Course
+                {
+                    CourseID = 1070,
+                    Title = "JavaScript",
+                    Hours = 987
+                },
+                new Course
+                {
+                    CourseID = 1080,
+                    Title = ".NET",
+                    Hours = 3
+                },
+                new Course
+                {
+                    CourseID = 1090,
+                    Title = "MVC",
+                    Hours = 81
+                },
+                new Course
+                {
+                    CourseID = 1100,
+                    Title = "Python",
+                    Hours = 99
+                },
+                new Course
+                {
+                    CourseID = 1120,
+                    Title = "BootStrap",
+                    Hours = 21
+                }
+            };
+
+            courses.ForEach(c => context.Courses.Add(c));
+            context.SaveChanges();
+
+            var enrollments = new List<Enrollment>
+            {
+                new Enrollment
+                {
+                    StudentID = 1,
+                    CourseID = 1050,
+                    Grade = Grade.A
+                },
+                new Enrollment
+                {
+                    StudentID = 1,
+                    CourseID = 1060,
+                    Grade = Grade.B
+                },
+                new Enrollment
+                {
+                    StudentID = 1,
+                    CourseID = 1070,
+                    Grade = Grade.A
+                },
+                new Enrollment
+                {
+                    StudentID = 2,
+                    CourseID = 1050,
+                    Grade = Grade.E
+                },
+                new Enrollment
+                {   StudentID = 2,
+                    CourseID = 1070,
+                    Grade = Grade.F
+                },
+                new Enrollment
+                {
+                    StudentID = 3,
+                    CourseID = 1050,
+                    Grade = Grade.C
+                },
+                new Enrollment
+                {
+                    StudentID = 2,
+                    CourseID = 1090,
+                    Grade = Grade.B
+                },
+                new Enrollment
+                {
+                    StudentID = 3,
+                    CourseID = 1070,
+                    Grade = Grade.D
+                },
+                new Enrollment
+                {
+                    StudentID = 4,
+                    CourseID = 1050
+                },
+                new Enrollment
+                {
+                    StudentID = 4,
+                    CourseID = 1070,
+                    Grade = Grade.C
+                },
+                new Enrollment
+                {
+                    StudentID = 4,
+                    CourseID = 1090,
+                    Grade = Grade.F
+                }
+            };
+
+            enrollments.ForEach(e => context.Enrollments.Add(e));
+            context.SaveChanges();
         }
     }
 }
